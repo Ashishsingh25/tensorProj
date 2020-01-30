653d5c38fff67e45be29fc49ad4b866e1ff071d6
AshishSingh25 <ashish.31325@gmail.com>
2020-01-30 23:07:26 +0530

    completed the test version of project

---
+++ wordEmb/testWordEmbedding.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import io
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+tf.enable_eager_execution
+
+(train_data, test_data), info = tfds.load(
+    'imdb_reviews/subwords8k',
+    split = (tfds.Split.TRAIN, tfds.Split.TEST),
+    with_info=True, as_supervised=True)
+
+encoder = info.features['text'].encoder
+# print(encoder.subwords[:20])
+
+padded_shapes = ([None],())
+train_batches = train_data.shuffle(1000).padded_batch(10, padded_shapes = padded_shapes)
+test_batches = test_data.shuffle(1000).padded_batch(10, padded_shapes = padded_shapes)
+
+embedding_dim=16
+
+model = keras.Sequential([
+  layers.Embedding(encoder.vocab_size, embedding_dim),
+  layers.GlobalAveragePooling1D(),
+  layers.Dense(16, activation='relu'),
+  layers.Dense(1, activation='sigmoid')
+])
+
+# model.summary()
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(
+    train_batches,
+    epochs=10,
+    validation_data=test_batches, validation_steps=20)
+
+model.save('test_model1.h5')
+
+# model = tf.keras.models.load_model('test_model1.h5')
+# model.summary()
+
+e = model.layers[0]
+weights = e.get_weights()[0]
+print(weights.shape) # shape: (vocab_size, embedding_dim)
+
+encoder = info.features['text'].encoder
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+
+for num, word in enumerate(encoder.subwords):
+  vec = weights[num+1] # skip 0, it's padding.
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in vec]) + "\n")
+out_v.close()
+out_m.close()
