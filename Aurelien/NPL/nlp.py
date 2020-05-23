af6c01b5eb5c24a4a72a123a2a800f247c480fd7
AshishSingh25 <ashish.31325@gmail.com>
2020-05-23 20:37:13 +0530

    Adding NLP examples

---
+++ Aurelien/NPL/nlp.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+shakespeare_url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+filepath = keras.utils.get_file("shakespeare.txt", shakespeare_url)
+with open(filepath) as f:
+    shakespeare_text = f.read()
+print(shakespeare_text[:148])
